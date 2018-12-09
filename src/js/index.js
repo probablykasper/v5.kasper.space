092e96852a8c50c3eed48ac07fec85b981cbe1cb
Kasper <kasperkh.kh@gmail.com>
2018-12-09 20:49:34 +0100

    Submenus in navbar instead

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,28 @@ document.addEventListener('click', (e) => {
     window.history.pushState({page:e.target.dataset.page}, 'KH - '+e.target.innerHTML, '/'+e.target.dataset.page)
   }
 })
+
+// menu
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('menu-item')) {
+    const menuItems = e.target.parentElement.children
+    let loopedPast = false
+    for (let i = 0; i < menuItems.length; i++) {
+      menuItems[i].classList.remove('left')
+      menuItems[i].classList.remove('right')
+      menuItems[i].classList.remove('current')
+      if (loopedPast) {
+        menuItems[i].classList.add('right')
+      } else if (menuItems[i] === e.target) {
+        menuItems[i].classList.add('current')
+        loopedPast = true
+      } else {
+        menuItems[i].classList.add('left')
+      }
+    }
+    const current = e.target.parentElement.querySelector('.current')
+    if (current) current.classList.remove('current')
+    e.target.classList.add('current')
+    // window.history.pushState({page:e.target.dataset.page}, 'KH - '+e.target.innerHTML, '/'+e.target.dataset.page)
+  }
+})
