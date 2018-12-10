8740c9aa8a98581705147aa645fe87156c8fbd8b
Kasper <kasperkh.kh@gmail.com>
2018-12-10 02:35:29 +0100

    Fixing stuff. Im bad at commit logs, jeez

--- src/js/index.js
+++ src/js/index.js
@@ -3,35 +3,74 @@ require('./canvas-renderer-and-projector.min.js')
 require('./bg.js')
 
 // pages
-document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('nav-item')) {
-    document.querySelector('.current-page').classList.remove('current-page')
-    e.target.classList.add('current-page')
-    window.history.pushState({page:e.target.dataset.page}, 'KH - '+e.target.innerHTML, '/'+e.target.dataset.page)
-  }
-})
+function openPage(newPage, title) {
+  document.title = title
+  const currentPageEl = document.querySelector('.page.visible')
+  if (currentPageEl) currentPageEl.classList.remove('visible')
+  const pageEl = document.querySelector(`.page[data-page='${newPage}']`)
+  pageEl.classList.add('visible')
+}
 
-// menu
 document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('menu-item')) {
-    const menuItems = e.target.parentElement.children
-    let loopedPast = false
-    for (let i = 0; i < menuItems.length; i++) {
-      menuItems[i].classList.remove('left')
-      menuItems[i].classList.remove('right')
-      menuItems[i].classList.remove('current')
-      if (loopedPast) {
-        menuItems[i].classList.add('right')
-      } else if (menuItems[i] === e.target) {
-        menuItems[i].classList.add('current')
-        loopedPast = true
-      } else {
-        menuItems[i].classList.add('left')
-      }
+  let el = e.target
+  while (el.parentElement && el.tagName !== 'A') el = el.parentElement
+  
+  if (el.tagName === 'A') {
+    e.preventDefault()
+    // deselect current page from nav bar
+    const navItems = document.querySelectorAll('.nav-bar .current')
+    for (let i = 0; i < navItems.length; i++) {
+      navItems[i].classList.remove('current')
     }
-    const current = e.target.parentElement.querySelector('.current')
-    if (current) current.classList.remove('current')
-    e.target.classList.add('current')
-    // window.history.pushState({page:e.target.dataset.page}, 'KH - '+e.target.innerHTML, '/'+e.target.dataset.page)
+    // select new page in nav bar
+    newPage = el.getAttribute('href')
+    navBarItem = document.querySelector(`.nav-bar a[href='${newPage}']`)
+    if (navBarItem) {
+      navBarItem.classList.add('current')
+      let title = el.innerText+' | KH'
+      if (el.classList.contains('in-submenu')) {
+        const parentNavItem = el.parentElement.parentElement
+        parentNavItem.classList.add('current')
+        const parentText = parentNavItem.children[0].innerText
+        console.log(parentText)
+        let title = el.innerText+' - '+el.parentElement.parentElement.innerText+' | KH'
+      }
+      window.history.pushState({page:newPage}, title, newPage)
+      openPage(newPage, title)
+    } else {
+      const title = 'KH'
+      window.history.pushState({page:newPage}, title, newPage)
+      openPage(newPage, title)
+    }
   }
-})
+
+  
+  // let el = e.target
+  // if (el.parentElement.classList.contains('nav-item')) el = el.parentElement
+  // if (el.classList.contains('nav-item')) {
+  //   const navItems = document.querySelectorAll('.nav-item.current')
+  //   for (let i = 0; i < navItems.length; i++) {
+  //     navItems[i].classList.remove('current')
+  //   }
+  //   el.classList.add('current')
+  //   let title = e.target.innerText.trim()+' | KH'
+  //   if (el.classList.contains('in-submenu')) {
+  //     el.parentElement.parentElement.classList.add('current')
+  //     title = el.innerHTML+' - '+el.parentElement.parentElement.innerHTML+' | '+title
+  //   }
+  //   const newPage = el.dataset.page
+  //   window.history.pushState({page:newPage}, title, newPage)
+  //   openPage(newPage, title)
+  // } else if (el.classList.contains('kh-logo')) {
+  //   const newPage = e.target.dataset.page
+  //   const title = 'KH'
+  //   window.history.pushState({page:newPage}, title, newPage)
+  //   openPage(newPage, title)
+  // }
+
+
+
+}, false)
+window.onpopstate = (e) => {
+  openPage(e.state.page)
+}
